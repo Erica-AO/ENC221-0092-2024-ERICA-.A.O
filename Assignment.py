4463c1a3d7aa8e51715e8acbe9903fb41c632ed5
Erica-AO <ericaouma01@gmail.com>
2025-11-24 00:42:27 +0300

    Add files via upload

---
+++ Assignment.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+class roommate:
+    def __init__ (self,height,size,complexion,hairstyle):
+        self.height=height
+        self.size=size
+        self.complexion=complexion
+        self.hairstyle=hairstyle
+
+
+# In[16]:
+
+
+Anyango=roommate(height="5.1ft",size="slim",complexion="chocolate",hairstyle="afro")
+
+
+# In[20]:
+
+
+Anyango.height
+
+
+# In[22]:
+
+
+Anyango.size
+
+
+# In[24]:
+
+
+Anyango.complexion
+
+
+# In[26]:
+
+
+Anyango.hairstyle
+
+
+# In[ ]:
+
+
+
+
